a29c03594e6a41ccbcf868ab15aa9df0886051f9
hetzlefetz <distorted.peace@gmail.com>
2022-05-08 12:05:11 +0200

    Implemented JSR1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 
+let listData = [];
+const dataUri = window.location.href.trimEnd().toLocaleLowerCase().replace("index.html", "") + "/data/listitems.json";
+
+if (typeof Element.prototype.clearChildren === 'undefined') {
+    Object.defineProperty(Element.prototype, 'clearChildren', {
+        configurable: true,
+        enumerable: false,
+        value: function () {
+            while (this.firstChild) this.removeChild(this.lastChild);
+        }
+    });
+}
+
 const switchView = async () => {
     var btn = document.getElementById("btn-viewSwitch");
     var list = document.getElementsByClassName("list")[0];
@@ -23,28 +36,68 @@ const listItemClick = (ev) => {
     alert(ev.target.closest("div.list-item").querySelector(".primary-text").textContent)
 }
 const optionClick = (ev) => {
-
     const url = ev.target.closest("div.list-item").querySelector(".cover").src;
     const title = ev.target.closest("div.list-item").querySelector(".primary-text").textContent;
     alert(`${url}\n${title}`)
     ev.preventDefault();
     ev.stopPropagation();
 }
+const refreshClick = async ()=>{
+    await refetchListData();
+    console.log("uhuh")
+}
 const attachHandlers = () => {
     document.getElementById("btn-viewSwitch").onclick = switchView
+    document.getElementById("btn-reload").onclick = refreshClick
+
+}
+
+const createItem = ({ title, src, numOfTags, added, owner }) => {
+    var element = document.createElement("div");
+    element.classList.add("list-item")
+    element.innerHTML = `<div class="column center">
+    <div class="cover-wrapper">
+        <img class="cover" src="${src}" alt="album cover">
+        <button class="bottom overlay-bottom-right btn-option">
+            <img class="inline-img-m" src="./assets/glyphicons-518-option-vertical.png"
+                alt="main menu icon" />
+        </button>
+    </div>
+</div>
+<div class="column grow">
+    <span>${owner}</span>
+    <span class="primary-text font-size-l">${title}</span>
+    <span class="bottom">▶${numOfTags}</span>
+</div>
+<div class="column right">
+    <span>${added}</span>
+    <button class="bottom btn-option">
+        <img class="inline-img-m" src="./assets/glyphicons-518-option-vertical.png"
+            alt="main menu icon" />
+    </button>
+</div>`
+    return element
+}
+const fillList = (data) => {
+    if (!data) {
+        data = listData;
+    }
+    const list = document.getElementsByClassName("list")[0]
+    list.clearChildren();
+    data.forEach(element => list.appendChild(createItem(element)))
     Array.from(document.getElementsByClassName("btn-option")).forEach(element => {
         element.onclick = optionClick
     });
     Array.from(document.getElementsByClassName("list-item")).forEach(element => {
         element.onclick = listItemClick
     });
+
 }
 
+const refetchListData = async () => await fetch(dataUri).then(response => response.json())
+    .then(data => { listData = data; fillList(data) });
+
 window.onload = async () => {
+    await refetchListData();
     attachHandlers();
-    var uri =
-        window.location.href.trimEnd().toLocaleLowerCase().replace("index.html", "");
-    uri += "/data/listitems.json";
-    await fetch(uri).then(response => response.json())
-        .then(data => fillList());
 };
